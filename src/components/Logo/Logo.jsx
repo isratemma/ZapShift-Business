3808c2fa7182f0a35fe2e1a623293e39d372b079
isratemma <emmaaissraatt@gmail.com>
2026-06-08 00:41:32 +0600

    6th commit

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../assets/brands/logo.png'
+
+const Logo = () => {
+  return (
+  <div>
+
+    <img src={logo} alt="logo" />
+
+    </div>
+  );
+};
+
+export default Logo;
