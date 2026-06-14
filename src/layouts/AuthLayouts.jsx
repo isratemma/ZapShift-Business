f6ef9f6b0351de56c44a73aad6bd5e6491487ce5
isratemma <emmaaissraatt@gmail.com>
2026-06-14 21:28:49 +0600

    Add AuthLayout with logo bar and side image

--- src/layouts/AuthLayouts.jsx
+++ src/layouts/AuthLayouts.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import Logo from '../components/Logo/Logo';
-import { Outlet } from 'react-router';
+import { Outlet, Link } from 'react-router';
 import authImage from '../assets/authImage.png';
+import Logo from '../components/Logo/Logo';
 
 const AuthLayouts = () => {
   return (
-    <div>
-      <Logo></Logo>
-      <div>
-        <div>
-          <Outlet></Outlet>
+    <div className="min-h-screen bg-[#f5f5f5] flex flex-col">
+      {/* Top logo bar */}
+      <div className="px-8 py-5 bg-white shadow-sm">
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+
+      {/* Main content */}
+      <div className="flex flex-1 items-center justify-center gap-12 px-6 py-12 md:px-16">
+        {/* Form side */}
+        <div className="w-full max-w-md">
+          <Outlet />
         </div>
-        <div>
-          <img src={authImage} alt="Auth Background" />
+
+        {/* Image side */}
+        <div className="hidden lg:block w-full max-w-md">
+          <img src={authImage} alt="Auth" className="w-full object-contain" />
         </div>
       </div>
     </div>
