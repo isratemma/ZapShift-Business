6d5fe9324be2bd0d2e40e53695b145a6f5186eea
isratemma <emmaaissraatt@gmail.com>
2026-06-18 20:44:42 +0600

    Add PrivateRoute: redirects to login if not authenticated

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <span className="loading loading-spinner loading-lg text-[#CAEB66]"></span>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/auth/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
